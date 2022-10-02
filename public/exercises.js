a7e7cf6c9c7dee295ce176ee2d51da05518907eb
Daniel Bray <daniel.bray@sonalake.com>
2022-10-02 10:40:40 +0100

    button works

--- public/exercises.js
+++ public/exercises.js
@@ -1,4 +1,10 @@
 const loadNextExercise  = () =>{
     $.get('exercise?' + $('#practice').serialize())
+    .then((exercise) => {
+        $('#tone').text(exercise.tone)
+        $('#scale').text(exercise.scale)
+        $('#exercise').text(exercise.exercise)
+
+    })
 
 }
