3301f2d27739667fbaef5e112da7105ac7deb890
Daniel Bray <daniel.bray@sonalake.com>
2022-10-02 09:25:13 +0100

    show configs

--- server.js
+++ server.js
@@ -42,22 +42,14 @@ if (seo.url === "glitch-default") {
  */
 fastify.get("/", function (request, reply) {
   // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
+  const config = require("./src/exercise-config.json");
 
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
+  // Add the color properties to the params object
+  const params = {
+    scales: config.scales,
+    exercises: config.exercises,
+    seo: seo,
+  };
 
   // The Handlebars code will be able to access the parameter values and build them into the page
   return reply.view("/src/pages/index.hbs", params);
