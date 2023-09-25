78e584fe5ad7ca23f9475bd2ec8cd5a1b55289ec
Daniel Bray <daniel.bray@sonalake.com>
2023-09-25 22:46:20 +0100

    added fifths

--- public/exercises.js
+++ public/exercises.js
@@ -1,79 +1,98 @@
-
-
 const clearLastExercise = () => {
-    $('#exercise-details').html(`
-    <div id = "exercise-loader" class="spinner-border" role="status">
-        <span class="sr-only"></span>
-    </div>
-    `)
-    $('#tone').empty()
-    $('#scale').empty()
-    $('#exercise').empty()
-    $('#notes').empty()
-    $('.fb-container').empty()   
-    $('#description').empty() 
-}
-
-const loadNextExercise  = () => {
-    clearLastExercise()
-    $.get('exercise?' + $('#practice').serialize())
-    .then((exercise) => {
-
-        const scale_name  = `${exercise.tone} ${exercise.scale.name}`
-        const scale_note_names = fretboard.asNotes(scale_name).toUpperCase()
-        
+    $("#exercise-details").html(`
+      <div id = "exercise-loader" class="spinner-border" role="status">
+          <span class="sr-only"></span>
+      </div>
+      `);
+    $("#tone").empty();
+    $("#scale").empty();
+    $("#exercise").empty();
+    $("#notes").empty();
+    $(".fb-container").empty();
+    $("#description").empty();
+  };
+  
+  const loadNextExercise = () => {
+    clearLastExercise();
+    $.get("exercise?" + $("#practice").serialize())
+      .then((exercise) => {
+        const scale_name = `${exercise.tone} ${exercise.scale.name}`;
+        const scale_note_names = fretboard.asNotes(scale_name).toUpperCase();
+  
         // workaround for an odd bug
-        const tonic = scale_note_names.split(' ').shift()
-        if (tonic === '') {
-            loadNextExercise()
+        const tonic = scale_note_names.split(" ").shift();
+        if (tonic === "") {
+          loadNextExercise();
         }
-
-        $('#exercise-details').html(`
-        <div id="exercise-details" >
-            <div class = "row">
-                <h1>${exercise.tone} ${exercise.scale.label} ${exercise.exercise.label}</h1>   
-                <span><i>${exercise.exercise.description}</small></i></span>  
-            </div>
-            <div class = "row">
-                <div class = "col">
-                    <p><div id="notes"></div></p>
-                    <p><div id="chords"></div></p>
-                </div>
-            </div>
-            <div class = "row">
-                <div class = "col-sm-2"><img width = "100%" src="circle-of-fifths.svg" /></div>
-                <div class = "col" id = "fb-container" 
-                        data-frets="23"  
-                        data-notes="${scale_name}" 
-                        data-showTitle="true">
-                </div>
-            </div>
-        </div>
-        `)   
-
-        const colors = ['red','green', 'blue', 'black', 'purple', 'grey', 'orange']
-        $('#notes').append($("<b>").text(`Notes: `))
-        scale_note_names.split(' ').forEach((tone, i) => {
-            $('#notes').append($(`<span style='color:${colors[i]}'>`).text(`${tone} `)) 
-        })
-
-        if ( exercise.chordBases) {
-            $('#chords').append($("<b>").text(`Diatonic chords: `))
-            scale_note_names.split(' ').forEach((tone, i) => {
-                $('#chords').append($(`<span style='color:${colors[i]}'>`).text(`${tone}${exercise.chordBases[i]} `)) 
-            })
-              
+  
+        const colors = [
+          "red",
+          "green",
+          "blue",
+          "black",
+          "purple",
+          "grey",
+          "orange",
+        ];
+  
+        $("#exercise-details").html(`
+          <div id="exercise-details" >
+              <div class = "row">
+                  <h1>${exercise.tone} ${exercise.scale.label} ${exercise.exercise.label}</h1>   
+                  <span><i>${exercise.exercise.description}</small></i></span>  
+              </div>
+              <div class = "row">
+                  <div class = "col">
+                      <div class = "row">
+                          <div class = "col">
+                              <p><div id="notes"></div></p>
+                              <p><b>Fifths: </b>
+                              <span style='color:${colors[3]}'>${exercise.fifthBelow}</span>
+                              &#8594;
+                              <span style='color:${colors[0]}'>${exercise.tone}</span> 
+                              &#8594;
+                              <span style='color:${colors[4]}'>${exercise.fifthAbove}</span> 
+                              </p>
+                              <p><div id="chords"></div></p>
+                          </div>
+                      </div>
+                      <div class = "row">
+                          <div class = "col" id = "fb-container" 
+                                  data-frets="23"  
+                                  data-notes="${scale_name}" 
+                                  data-showTitle="true">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          `);
+  
+        $("#notes").append($("<b>").text(`Notes: `));
+        scale_note_names.split(" ").forEach((tone, i) => {
+          $("#notes").append(
+            $(`<span style='color:${colors[i]}'>`).text(`${tone} `)
+          );
+        });
+  
+        if (exercise.chordBases) {
+          $("#chords").append($("<b>").text(`Diatonic chords: `));
+          scale_note_names.split(" ").forEach((tone, i) => {
+            $("#chords").append(
+              $(`<span style='color:${colors[i]}'>`).text(
+                `${tone}${exercise.chordBases[i]} `
+              )
+            );
+          });
         }
-        
-        fretboard.Fretboard.drawAll('#fb-container', {
-            'leftHanded': $('#leftHanded').is(":checked"),
-            'fretWidth':30
-        })
-
-
-    })
-    .fail((error) => {
-        console.log(`Failed: ${JSON.stringify(error)}`)
-    })
-
-}
+  
+        fretboard.Fretboard.drawAll("#fb-container", {
+          leftHanded: $("#leftHanded").is(":checked"),
+          fretWidth: 30,
+        });
+      })
+      .fail((error) => {
+        console.log(`Failed: ${JSON.stringify(error)}`);
+      });
+  };
+  
--- server.js
+++ server.js
@@ -1,129 +1,143 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+  /**
+   * This is the main Node.js server script for your project
+   * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+   */
 
-const path = require("path");
+  const path = require("path");
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+  // Require the fastify framework and instantiate it
+  const fastify = require("fastify")({
+    // Set this to true for detailed logging:
+    logger: false,
+  });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+  // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
+  // Setup our static files
+  fastify.register(require("@fastify/static"), {
+    root: path.join(__dirname, "public"),
+    prefix: "/", // optional: default '/'
+  });
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
+  // Formbody lets us parse incoming forms
+  fastify.register(require("@fastify/formbody"));
 
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
+  // View is a templating manager for fastify
+  fastify.register(require("@fastify/view"), {
+    engine: {
+      handlebars: require("handlebars"),
+    },
+  });
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
+  // Load and parse SEO data
+  const seo = require("./src/seo.json");
+  if (seo.url === "glitch-default") {
+    seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+  }
 
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  const config = require("./src/exercise-config.json");
+  /**
+   * Our home page route
+   *
+   * Returns src/pages/index.hbs with data built into it
+   */
+  fastify.get("/", function (request, reply) {
+    // params is an object we'll pass to our handlebars template
+    const config = require("./src/exercise-config.json");
 
-  // Add the color properties to the params object
-  const params = {
-    scales: config.scales,
-    exercises: config.exercises,
-    seo: seo,
+    // Add the color properties to the params object
+    const params = {
+      scales: config.scales,
+      exercises: config.exercises,
+      seo: seo,
+    };
+
+    // The Handlebars code will be able to access the parameter values and build them into the page
+    return reply.view("/src/pages/index.hbs", params);
+  });
+
+  fastify.get("/exercise", function (request, reply) {
+    const plan = derive_plan(request);
+    const exercise = derive_exercise(plan);
+    return reply.send(exercise);
+  });
+
+  // Run the server and report out to the logs
+  fastify.listen(
+    { port: process.env.PORT, host: "0.0.0.0" },
+    function (err, address) {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+      console.log(`Your app is listening on ${address}`);
+      fastify.log.info(`server listening on ${address}`);
+    }
+  );
+
+  const derive_plan = (request) => {
+    const plan = {
+      scales: [],
+      exercises: [],
+    };
+
+    Object.keys(request.query).forEach(function (key) {
+      if (key.startsWith("scale_") && request.query[key] == "on") {
+        plan.scales.push(key.substring(key.indexOf("_") + 1));
+      }
+
+      if (key.startsWith("exercise_") && request.query[key] == "on") {
+        plan.exercises.push(key.substring(key.indexOf("_") + 1));
+      }
+    });
+
+    return plan;
   };
 
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
+  const musicomatic = require("musicomatic");
+  const derive_exercise = (plan) => {
+    const config = require("./src/exercise-config.json");
 
+    const selected_scale =
+      plan.scales[Math.floor(Math.random() * plan.scales.length)];
+    const selected_exercise =
+      plan.exercises[Math.floor(Math.random() * plan.exercises.length)];
 
-
-fastify.get("/exercise", function (request, reply) {
-
-
-  const plan = derive_plan(request)
-  const exercise = derive_exercise(plan)
-  return reply.send(exercise);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
+    let modeName = null;
+    switch (selected_scale) {
+      case "major":
+        modeName = "Ionian";
+        break;
+      default:
+        modeName = musicomatic.MODE_NAMES.find(
+          (c) => c.toLowerCase() == selected_scale.toLowerCase()
+        );
     }
-    console.log(`Your app is listening on ${address}`);
-    fastify.log.info(`server listening on ${address}`);
-  }
-);
+    let chordBases = null;
+    let chordNumerals = null;
 
+    const tone = config.tones[Math.floor(Math.random() * config.tones.length)];
 
-const derive_plan = (request) => {
-  const plan = {
-    scales: [],
-    exercises: []
-  }
-  
-  Object.keys(request.query).forEach(function(key) {
-    if (key.startsWith('scale_') && request.query[key] == 'on') {
-      plan.scales.push(key.substring(key.indexOf('_') + 1))
+    let mode;
+    if (modeName) {
+      mode = musicomatic.mode(modeName);
+      chordBases = mode.chordBases;
+      chordNumerals = mode.chordNumerals;
     }
 
-    if (key.startsWith('exercise_') && request.query[key] == 'on') {
-      plan.exercises.push(key.substring(key.indexOf('_') + 1))
-    }
-  })
+    const majorMode = musicomatic.mode("Ionian").keyByTonic(tone);
+    const fifthAbove = majorMode.notesByDegree["5"];
+    const fifthBelow = majorMode.notesByDegree["4"];
 
-  return plan
-}
+    const exercise = {
+      tone: tone,
+      scale: config.scales.find((c) => c.name === selected_scale),
+      exercise: config.exercises.find((c) => c.name === selected_exercise),
+      chordBases: chordBases,
+      fifthAbove: fifthAbove,
+      fifthBelow: fifthBelow,
+      chordNumerals: chordNumerals,
+      modeName: modeName,
+    };
 
-
-const musicomatic = require('musicomatic')
-const derive_exercise = (plan) => {
-  const config = require("./src/exercise-config.json");
-
-  const selected_scale = plan.scales[Math.floor(Math.random() * plan.scales.length)]
-  const selected_exercise = plan.exercises[Math.floor(Math.random() * plan.exercises.length)]
-
-  let modeName = null;
-  switch (selected_scale) {
-    case 'major':
-      modeName = 'Ionian'
-      break
-    default:
-      modeName = musicomatic.MODE_NAMES.find( c => c.toLowerCase() == selected_scale.toLowerCase())
+    return exercise;
   }
-  let chordBases = null
-  if (modeName) {
-    chordBases = musicomatic.mode(modeName).chordBases 
-  }
-  const exercise = {
-    tone: config.tones[Math.floor(Math.random() * config.tones.length)],
-    scale: config.scales.find( c => c.name === selected_scale),
-    exercise: config.exercises.find( c => c.name === selected_exercise),
-    chordBases: chordBases
-  }
-
-  return exercise
-}
