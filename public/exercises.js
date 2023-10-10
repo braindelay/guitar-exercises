e86f30fa96d54a142c2c284539a64648a74e723a
Daniel Bray <daniel.bray@sonalake.com>
2023-10-10 18:07:54 +0100

    comments

--- public/exercises.js
+++ public/exercises.js
@@ -1,44 +1,32 @@
+// called when the page is fully loaded, to set stuff up
 const preparePage = () => {
   prepareTunings();
   prepareExerciseButtons();
-};
+}
 
+// when the page is loaded, update the practice setup form to have the tunings we support
 const prepareTunings = () => {
   Object.keys(fretboard.Tunings.guitar6).forEach((t) => {
     $("#tuning").append($(`<option value="${t}">${t}</option>`));
   });
-};
+}
+
+//  when the page is loaded, update the choose exercise form
 const prepareExerciseButtons = () => {
-  $("#selectedTone").on("change", () => loadNextExercise(true));
-  $("#selectedScale").on("change", () => loadNextExercise(true));
-  $("#selectedExercise").on("change", () => loadNextExercise(true));
-};
+  $(".chooseExercise").on("change", () => loadNextExercise(true))
+}
 
-const clearLastExercise = () => {
-  $("#tone").empty();
-  $("#updatedTone").empty();
-  $("#scale").empty();
-  $("#exercise").empty();
-  $("#notes").empty();
-  $(".fb-container").empty();
-  $("#description").empty();
-  $(".fretboard").remove();
-  $("#chords").empty();
 
-  $("#manualTone").val(null);
-  $("#manualScale").val(null);
-  $("#manualExercise").val(null);
-};
-
-const toggleLoading = (isLoading) => {
-  $("#nextExercise").attr("disabled", isLoading);
-  $(".show-when-loading").attr("hidden", !isLoading);
-  $(".hide-when-loading").attr("hidden", isLoading);
-};
-
+/**
+ * Called when we want to reload the exercise
+ * 
+ * @param {*} useSelection  if set, then we use the chooseExercise values, otherwise we ask for a random one
+ */
 const loadNextExercise = (useSelection) => {
   toggleLoading(true);
-  $('.collapseOnNextExercise').collapse('hide')
+  if (!useSelection) {
+    $('.collapseOnNextExercise').collapse('hide')
+  }
   
   clearLastExercise();
 
@@ -48,84 +36,115 @@ const loadNextExercise = (useSelection) => {
     $("#manualExercise").val($("#selectedExercise option:selected").val());
   }
 
-  const practicePath = $("#practice").serialize();
+  const practicePath = $("#practice").serialize()
   $.get(`exercise?${practicePath}`)
     .then((exercise) => {
       // aren't timing issues fun
-      clearLastExercise();
-      const scale_name = `${exercise.tone} ${exercise.scale.name}`;
+      clearLastExercise()
+      const scale_name = `${exercise.tone} ${exercise.scale.name}`
       const scale_note_names = exercise.scaleValues
         ? exercise.scaleValues
-        : fretboard.asNotes(scale_name).toUpperCase().split(" ");
+        : fretboard.asNotes(scale_name).toUpperCase().split(" ")
 
-      const tuning = $("#tuning").val() ? $("#tuning").val() : "standard";
+      const tuning = $("#tuning").val() ? $("#tuning").val() : "standard"
 
-      updateExerciseText(exercise, tuning);
-      renderNotesAndChordBases(exercise, scale_note_names);
-      renderFretboard(scale_name, tuning);
-      renderStaff(scale_note_names);
+      updateExerciseText(exercise, tuning)
+      renderNotesAndChordBases(exercise, scale_note_names)
+      renderFretboard(scale_name, tuning)
+      renderStaff(scale_note_names)
 
-      toggleLoading(false);
+      toggleLoading(false)
     })
     .fail((error) => {
-      console.log(`Failed: ${JSON.stringify(error)}`);
-    });
-};
+      console.log(`Failed: ${JSON.stringify(error)}`)
+    })
+}
 
+
+// hide or show panels while we load
+const toggleLoading = (isLoading) => {
+  $("#nextExercise").attr("disabled", isLoading)
+  $(".show-when-loading").attr("hidden", !isLoading)
+  $(".hide-when-loading").attr("hidden", isLoading)
+}
+
+
+// wipe all the old data
+const clearLastExercise = () => {
+  $("#tone").empty()
+  $("#updatedTone").empty()
+  $("#scale").empty()
+  $("#exercise").empty()
+  $("#notes").empty()
+  $(".fb-container").empty()
+  $("#description").empty()
+  $(".fretboard").remove()
+  $("#chords").empty()
+  $("#manualTone").val(null)
+  $("#manualScale").val(null)
+  $("#manualExercise").val(null)
+}
+
+
+// set exercise labels
 const updateExerciseText = (exercise, tuning) => {
-  $("#exerciseDescription").html(exercise.exercise.description);
-  $("#fifthBelow").html(exercise.fifthBelow);
-  $("#fifthTone").html(exercise.tone);
-  $("#fifthAbove").html(exercise.fifthAbove);
-  $("#tuningDescription").html(tuning);
+  $("#exerciseDescription").html(exercise.exercise.description)
+  $("#fifthBelow").html(exercise.fifthBelow)
+  $("#fifthTone").html(exercise.tone)
+  $("#fifthAbove").html(exercise.fifthAbove)
+  $("#tuningDescription").html(tuning)
 
-  $("#selectedTone").val(exercise.tone);
-  $("#selectedScale").val(exercise.scale.name);
-  $("#selectedExercise").val(exercise.exercise.name);
+  $("#selectedTone").val(exercise.tone)
+  $("#selectedScale").val(exercise.scale.name)
+  $("#selectedExercise").val(exercise.exercise.name)
 
-  $("#headerTone").html(exercise.tone);
-  $("#headerScale").html(exercise.scale.label);
-  $("#headerExercise").html(exercise.exercise.label);
-};
+  $("#headerTone").html(exercise.tone)
+  $("#headerScale").html(exercise.scale.label)
+  $("#headerExercise").html(exercise.exercise.label)
+}
 
-
+// show the notes in the scale, and, if meaningful, the diatonic chords
 const renderNotesAndChordBases = (exercise, scale_note_names) => {
   const colours = [
     "red", "green", "blue", "black", "purple", "grey", "orange",
     "maroon", "fuschia", "lime", "teal", "aqua"
-  ];
+  ]
 
-  $("#notes").append($("<b>").text(`Notes: `));
+  $("#notes").append($("<b>").text(`Notes: `))
   scale_note_names.forEach((tone, i) => {
     $("#notes").append(
       $(`<span style='color:${colours[i]}'>`).text(`${tone} `)
-    );
-  });
+    )
+  })
 
   if (exercise.chordBases) {
-    $("#chords").append($("<b>").text(`Diatonic chords: `));
+    $("#chords").append($("<b>").text(`Diatonic chords: `))
     scale_note_names.forEach((tone, i) => {
       $("#chords").append(
         $(`<span style='color:${colours[i]}'>`).text(
           `${tone}${exercise.chordBases[i]} `
         )
-      );
-    });
+      )
+    })
   }
-};
+}
 
 const renderFretboard = (scale_name, tuning) => {
-  $("#fb-container").attr("data-notes", scale_name);
+  $("#fb-container").attr("data-notes", scale_name)
   fretboard.Fretboard.drawAll("#fb-container", {
     leftHanded: $("#leftHanded").is(":checked"),
     fretWidth: 30,
     fretHeight: 20,
     showTitle: true,
     tuning: fretboard.Tunings.guitar6[tuning],
-  });
-};
+  })
+}
 
+// translates the scales into ABC format, and then
+// passes it to ABCJS to render
 const renderStaff = (scale_note_names) => {
+  // given the start of the scale, which notes 
+  // do we want to show in the higher end of the staff
   const flipNotes = {
     A: 'CDEFG',
     B: 'CDEFGA',
@@ -138,24 +157,28 @@ const renderStaff = (scale_note_names) => {
 
   const firstNote = scale_note_names[0].charAt(0)
   const staffNotes = scale_note_names.map((note, index) => {
-    const staffNote = note.charAt(0);
+  const staffNote = note.charAt(0);
+
+    // if we're in a flip note, then put it to lowercase
+    // so it appears in the higher register of the scale
     let appliedStaffNote = 
       flipNotes[firstNote].includes(staffNote) 
       ? staffNote.toLowerCase()
       : staffNote
 
+    // translate the accidental to ABC format
     let accidental = "";
     if (note.endsWith("bb")) {
-      accidental = "__";
+      accidental = "__"
     } else if (note.endsWith("b")) {
-      accidental = "_";
+      accidental = "_"
     } else if (note.endsWith("##")) {
-      accidental = "^^";
+      accidental = "^^"
     } else if (note.endsWith("#")) {
-      accidental = "^";
+      accidental = "^"
     }
-    return `${accidental}${appliedStaffNote}`;
-  });
+    return `${accidental}${appliedStaffNote}`
+  })
 
   var abcString = `    
   L:1/4
